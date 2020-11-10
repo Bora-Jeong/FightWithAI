6022b20fd9fd339dfbb2af2da161459ec6025f03
Bora-Jeong <50454596+Bora-Jeong@users.noreply.github.com>
2020-11-11 03:42:32 +0900

    Begin

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Dialogue
+{
+    public int bgIndex;
+    public string content;
+}
+
+public class GameManager : SingleTon<GameManager>
+{
+    [SerializeField] DialoguePanel _dialoguePanel;
+    [SerializeField] GameObject _gamePanel;
+
+    [Header("Prologue")]
+    [SerializeField] Sprite[] _prologueBG;
+    [SerializeField] Dialogue[] _dialogues;
+
+    [Header("Game")]
+    [SerializeField] Text _dayText;
+    [SerializeField] Slider _timeSlider;
+    [SerializeField] Text _timeText;
+    [SerializeField] Text _playerScoreText;
+    [SerializeField] Text _aiScoreText;
+
+    private int _day;
+    private float _totalTime;
+    private float _remainTime;
+    private int _playerScore;
+    private int _aiScore;
+
+    public bool isPlaying { get; private set; } // 게임 중?
+    public int playerScore
+    {
+        get => _playerScore;
+        set
+        {
+            _playerScore = value;
+            _playerScoreText.text = $"Score {_playerScore}";
+        }
+    }
+
+    public int aiScore
+    {
+        get => _aiScore;
+        set
+        {
+            _aiScore = value;
+            _aiScoreText.text = $"Score {_aiScore}";
+        }
+    }
+
+    public float remainTime
+    {
+        get => _remainTime;
+        set
+        {
+            _remainTime = value;
+            _timeText.text = $"{_remainTime}초";
+            _timeSlider.value = _remainTime / _totalTime;
+        }
+    }
+
+    private void Awake()
+    {
+        StartCoroutine(StartPrologue());
+    }
+
+    IEnumerator StartPrologue()  // 프롤로그 시작
+    {
+        _gamePanel.gameObject.SetActive(false);
+        _dialoguePanel.gameObject.SetActive(true);
+        for (int i = 0; i < _dialogues.Length; i++)
+        {
+            _dialoguePanel.Set(_prologueBG[_dialogues[i].bgIndex], _dialogues[i].content);
+            yield return new WaitForSeconds(2f);
+        }
+
+        _dialoguePanel.gameObject.SetActive(false);
+        _gamePanel.gameObject.SetActive(true);
+        RoundStart(1, 60f);
+    }
+
+    private void RoundStart(int day, float time) // 라운드 시작
+    {
+        _day = day;
+        _dayText.text = $"{_day}일차";
+        _totalTime = time;
+        remainTime = _totalTime;
+        playerScore = 0;
+        aiScore = 0;
+
+        isPlaying = true;
+        StartCoroutine(GameSchedulling());
+    }
+
+    IEnumerator GameSchedulling()
+    {
+        while(remainTime > 0)
+        {
+            remainTime -= Time.deltaTime;
+            yield return null;
+        }
+
+        GameOver();
+    }
+
+    private void GameOver() // 게임 오버
+    {
+        isPlaying = false;
+    }
+}
---
+++ Assets/Scripts/UI/DialoguePanel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialoguePanel : MonoBehaviour
+{
+    [SerializeField]
+    private Image _background;
+    [SerializeField]
+    private Text _text;
+
+    public void Set(Sprite sprite, string content)
+    {
+        _background.sprite = sprite;
+        _text.text = content;
+    }
+}
