77c70e4cd2c67283fa5e539dabb1cbf425654384
Bora-Jeong <50454596+Bora-Jeong@users.noreply.github.com>
2020-11-14 05:54:22 +0900

    recipe

---
+++ Assets/Scripts/Recipe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipe : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite normal;
+    [SerializeField]
+    private Sprite broken;
+
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void Hide()
+    {
+        StartCoroutine(StartHide());
+    }
+
+    private IEnumerator StartHide()
+    {
+        sr.sprite = broken;
+
+        yield return new WaitForSeconds(5f);
+
+        sr.sprite = normal;
+    }
+}
